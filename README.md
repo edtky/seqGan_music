c7f72575375697c00c315887acbce4f61d891da1
Edward Tiong <edwardtiong.63@gmail.com>
2018-12-22 01:04:53 +0800

    encoded music gen

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# seqGAN
-A PyTorch implementation of "SeqGAN: Sequence Generative Adversarial Nets with Policy Gradient." (Yu, Lantao, et al.). The code is highly simplified, commented and (hopefully) straightforward to understand. The policy gradients implemented are also much simpler than in the original work (https://github.com/LantaoYu/SeqGAN/) and do not involve rollouts- a single reward is used for the entire sentence (inspired by the examples in http://karpathy.github.io/2016/05/31/rl/).
+# seqGAN for Music Generation
+A PyTorch implementation of "SeqGAN: Sequence Generative Adversarial Nets with Policy Gradient." (Yu, Lantao, et al.). 
+
+Here we treat music generation as a language modeling problem and use SeqGAN model trained on encoded classical music by Mozart to generate new music.
+
+This project builds on the seqGAN implementation in https://github.com/suragnair/seqGAN that implements policy gradients much simpler than the original work and do not invole rollouts. 
 
 The architectures used are different than those in the orignal work. Specifically, a recurrent bidirectional GRU network is used as the discriminator. 
 
 The code performs the experiment on synthetic data as described in the paper.
-
-You are encouraged to raise any doubts regarding the working of the code as Issues.
 
 To run the code:
 ```bash 
@@ -13,8 +15,10 @@ python main.py
 ```
 main.py should be your entry point into the code.
 
-## Hacks and Observations
-The following hacks (borrowed from https://github.com/soumith/ganhacks) seem to have worked in this case:
+## Key observations and modifications
+- Instead of using an oracle which represents the true distribution, midi files of Mozart music are encoded into text as the data sample representing the true distribution
+- Vocabulary size substantially decreased from 5000 to about 149
+- Allowing the generator to create samples of flexible length
 - Training Discriminator a lot more than Generator (Generator is trained only for one batch of examples, and increasing the batch size hurts stability)
 - Using Adam for Generator and Adagrad for Discriminator
 - Tweaking learning rate for Generator in GAN phase
@@ -22,8 +26,3 @@ The following hacks (borrowed from https://github.com/soumith/ganhacks) seem to 
 
 - Stablity is extremely sensitive to almost every parameter :/
 - The GAN phase may not always lead to massive drops in NLL (sometimes very minimal) - I suspect this is due to the very crude nature of the policy gradients implemented (without rollouts).
-
-## Sample Learning Curve
-Learning curve obtained after MLE training for 100 epochs followed by adversarial training. (Your results may vary!)
-
-![alt tag](https://raw.githubusercontent.com/suragnair/seqGAN/master/learning_curve.png)
--- main.py
+++ main.py
@@ -10,41 +10,79 @@ import torch.nn as nn
 
 import generator
 import discriminator
-import helpers
+import music_helpers
+
+import random
 
 
 CUDA = False
-VOCAB_SIZE = 5000
+# VOCAB_SIZE = 5000
 MAX_SEQ_LEN = 20
 START_LETTER = 0
 BATCH_SIZE = 32
 MLE_TRAIN_EPOCHS = 100
 ADV_TRAIN_EPOCHS = 50
-POS_NEG_SAMPLES = 10000
+POS_NEG_SAMPLES = 10000   # consider increasing to 500k
+N_SAMPLES = 5
 
 GEN_EMBEDDING_DIM = 32
 GEN_HIDDEN_DIM = 32
 DIS_EMBEDDING_DIM = 64
 DIS_HIDDEN_DIM = 64
 
-oracle_samples_path = './oracle_samples.trc'
-oracle_state_dict_path = './oracle_EMBDIM32_HIDDENDIM32_VOCAB5000_MAXSEQLEN20.trc'
-pretrained_gen_path = './gen_MLEtrain_EMBDIM32_HIDDENDIM32_VOCAB5000_MAXSEQLEN20.trc'
-pretrained_dis_path = './dis_pretrain_EMBDIM_64_HIDDENDIM64_VOCAB5000_MAXSEQLEN20.trc'
+def load_music_file(file):
+    with open(file, 'r') as file:
+        text = file.read()
+        
+    # get vocabulary set
+    words = sorted(tuple(set(text.split())))
+    n = len(words)
 
+    # create word-integer encoder/decoder
+    word2int = dict(zip(words, list(range(n))))
+    int2word = dict(zip(list(range(n)), words))
 
-def train_generator_MLE(gen, gen_opt, oracle, real_data_samples, epochs):
+    # encode all words in dataset into integers
+    encoded = np.array([word2int[word] for word in text.split()])
+    
+    return n, word2int, int2word, encoded
+
+def batch_music_samples(data, seq_len):
+    batches = []
+    i=0
+    while i+seq_len <= len(data):
+        batch = list(data[i:i+seq_len])
+        batches.append(batch)
+        i += seq_len
+    return batches
+    
+def train_val_split(data, train_size):
+    random.seed(1)
+    random.shuffle(data)
+    train = torch.tensor(data[:train_size]).type(torch.LongTensor)
+    val = torch.tensor(data[train_size:]).type(torch.LongTensor)
+    return train, val
+
+def positive_sample(data, n):
+    torch.tensor(data)
+    sample_idx = random.sample(range(1, len(data)), n)
+    return data[sample_idx]   
+
+def train_generator_MLE(gen, gen_opt, real_data_samples, epochs):
     """
     Max Likelihood Pretraining for the generator
     """
     for epoch in range(epochs):
+        
         print('epoch %d : ' % (epoch + 1), end='')
         sys.stdout.flush()
         total_loss = 0
 
         for i in range(0, POS_NEG_SAMPLES, BATCH_SIZE):
-            inp, target = helpers.prepare_generator_batch(real_data_samples[i:i + BATCH_SIZE], start_letter=START_LETTER,
+            
+            inp, target = music_helpers.prepare_generator_batch(real_data_samples[i:i + BATCH_SIZE], start_letter=START_LETTER,
                                                           gpu=CUDA)
+            
             gen_opt.zero_grad()
             loss = gen.batchNLLLoss(inp, target)
             loss.backward()
@@ -57,53 +95,44 @@ def train_generator_MLE(gen, gen_opt, oracle, real_data_samples, epochs):
                 print('.', end='')
                 sys.stdout.flush()
 
-        # each loss in a batch is loss per sample
         total_loss = total_loss / ceil(POS_NEG_SAMPLES / float(BATCH_SIZE)) / MAX_SEQ_LEN
 
-        # sample from generator and compute oracle NLL
-        oracle_loss = helpers.batchwise_oracle_nll(gen, oracle, POS_NEG_SAMPLES, BATCH_SIZE, MAX_SEQ_LEN,
-                                                   start_letter=START_LETTER, gpu=CUDA)
-
-        print(' average_train_NLL = %.4f, oracle_sample_NLL = %.4f' % (total_loss, oracle_loss))
+        print(' average_train_NLL = %.4f' % (total_loss))
 
 
-def train_generator_PG(gen, gen_opt, oracle, dis, num_batches):
+def train_generator_PG(gen, gen_opt, dis, num_batches):
     """
     The generator is trained using policy gradients, using the reward from the discriminator.
     Training is done for num_batches batches.
     """
 
     for batch in range(num_batches):
-        s = gen.sample(BATCH_SIZE*2)        # 64 works best
-        inp, target = helpers.prepare_generator_batch(s, start_letter=START_LETTER, gpu=CUDA)
+        
+        s = gen.sample(BATCH_SIZE*2)        # 64 works best, try large to leverage more GPU
+        inp, target = music_helpers.prepare_generator_batch(s, start_letter=START_LETTER, gpu=CUDA)
         rewards = dis.batchClassify(target)
-
+        
         gen_opt.zero_grad()
         pg_loss = gen.batchPGLoss(inp, target, rewards)
         pg_loss.backward()
         gen_opt.step()
 
-    # sample from generator and compute oracle NLL
-    oracle_loss = helpers.batchwise_oracle_nll(gen, oracle, POS_NEG_SAMPLES, BATCH_SIZE, MAX_SEQ_LEN,
-                                                   start_letter=START_LETTER, gpu=CUDA)
-
-    print(' oracle_sample_NLL = %.4f' % oracle_loss)
-
-
-def train_discriminator(discriminator, dis_opt, real_data_samples, generator, oracle, d_steps, epochs):
+def train_discriminator(discriminator, dis_opt, real_data_samples, generator, real_val, d_steps, epochs):
     """
     Training the discriminator on real_data_samples (positive) and generated samples from generator (negative).
     Samples are drawn d_steps times, and the discriminator is trained for epochs epochs.
     """
-
-    # generating a small validation set before training (using oracle and generator)
-    pos_val = oracle.sample(100)
+    
+    pos_val = positive_sample(real_val, 100)
     neg_val = generator.sample(100)
-    val_inp, val_target = helpers.prepare_discriminator_data(pos_val, neg_val, gpu=CUDA)
+    val_inp, val_target = music_helpers.prepare_discriminator_data(pos_val, neg_val, gpu=CUDA)
 
     for d_step in range(d_steps):
-        s = helpers.batchwise_sample(generator, POS_NEG_SAMPLES, BATCH_SIZE)
-        dis_inp, dis_target = helpers.prepare_discriminator_data(real_data_samples, s, gpu=CUDA)
+        
+        s = music_helpers.batchwise_sample(generator, POS_NEG_SAMPLES, BATCH_SIZE)
+        
+        dis_inp, dis_target = music_helpers.prepare_discriminator_data(real_data_samples, s, gpu=CUDA)
+        
         for epoch in range(epochs):
             print('d-step %d epoch %d : ' % (d_step + 1, epoch + 1), end='')
             sys.stdout.flush()
@@ -111,7 +140,9 @@ def train_discriminator(discriminator, dis_opt, real_data_samples, generator, or
             total_acc = 0
 
             for i in range(0, 2 * POS_NEG_SAMPLES, BATCH_SIZE):
+                
                 inp, target = dis_inp[i:i + BATCH_SIZE], dis_target[i:i + BATCH_SIZE]
+                
                 dis_opt.zero_grad()
                 out = discriminator.batchClassify(inp)
                 loss_fn = nn.BCELoss()
@@ -136,50 +167,55 @@ def train_discriminator(discriminator, dis_opt, real_data_samples, generator, or
 
 # MAIN
 if __name__ == '__main__':
-    oracle = generator.Generator(GEN_EMBEDDING_DIM, GEN_HIDDEN_DIM, VOCAB_SIZE, MAX_SEQ_LEN, gpu=CUDA)
-    oracle.load_state_dict(torch.load(oracle_state_dict_path))
-    oracle_samples = torch.load(oracle_samples_path).type(torch.LongTensor)
-    # a new oracle can be generated by passing oracle_init=True in the generator constructor
-    # samples for the new oracle can be generated using helpers.batchwise_sample()
+    
+    # prepare train and val data
+    mozart_data = "./data/mozart.txt"
+    VOCAB_SIZE, word2int, int2word, encoded_data = load_music_file(mozart_data)
+    real_data_samples = batch_music_samples(encoded_data, MAX_SEQ_LEN)
+    real_train, real_val = train_val_split(real_data_samples, POS_NEG_SAMPLES)
 
     gen = generator.Generator(GEN_EMBEDDING_DIM, GEN_HIDDEN_DIM, VOCAB_SIZE, MAX_SEQ_LEN, gpu=CUDA)
     dis = discriminator.Discriminator(DIS_EMBEDDING_DIM, DIS_HIDDEN_DIM, VOCAB_SIZE, MAX_SEQ_LEN, gpu=CUDA)
 
     if CUDA:
-        oracle = oracle.cuda()
         gen = gen.cuda()
         dis = dis.cuda()
-        oracle_samples = oracle_samples.cuda()
+        real_train = real_train.cuda()
+        real_val = real_val.cuda()
 
     # GENERATOR MLE TRAINING
     print('Starting Generator MLE Training...')
     gen_optimizer = optim.Adam(gen.parameters(), lr=1e-2)
     train_generator_MLE(gen, gen_optimizer, oracle, oracle_samples, MLE_TRAIN_EPOCHS)
 
-    # torch.save(gen.state_dict(), pretrained_gen_path)
-    # gen.load_state_dict(torch.load(pretrained_gen_path))
-
     # PRETRAIN DISCRIMINATOR
     print('\nStarting Discriminator Training...')
     dis_optimizer = optim.Adagrad(dis.parameters())
     train_discriminator(dis, dis_optimizer, oracle_samples, gen, oracle, 50, 3)
 
-    # torch.save(dis.state_dict(), pretrained_dis_path)
-    # dis.load_state_dict(torch.load(pretrained_dis_path))
-
     # ADVERSARIAL TRAINING
     print('\nStarting Adversarial Training...')
-    oracle_loss = helpers.batchwise_oracle_nll(gen, oracle, POS_NEG_SAMPLES, BATCH_SIZE, MAX_SEQ_LEN,
-                                               start_letter=START_LETTER, gpu=CUDA)
-    print('\nInitial Oracle Sample Loss : %.4f' % oracle_loss)
+    # oracle_loss = helpers.batchwise_oracle_nll(gen, oracle, POS_NEG_SAMPLES, BATCH_SIZE, MAX_SEQ_LEN,
+    #                                            start_letter=START_LETTER, gpu=CUDA)
+    # print('\nInitial Oracle Sample Loss : %.4f' % oracle_loss)
 
     for epoch in range(ADV_TRAIN_EPOCHS):
         print('\n--------\nEPOCH %d\n--------' % (epoch+1))
         # TRAIN GENERATOR
-        print('\nAdversarial Training Generator : ', end='')
+        print('\nAdversarial Training Generator: ', end='')
         sys.stdout.flush()
-        train_generator_PG(gen, gen_optimizer, oracle, dis, 1)
+        train_generator_PG(gen, gen_optimizer, dis, 1)
 
         # TRAIN DISCRIMINATOR
         print('\nAdversarial Training Discriminator : ')
-        train_discriminator(dis, dis_optimizer, oracle_samples, gen, oracle, 5, 3)
+        train_discriminator(dis, dis_optimizer, real_train, gen, real_val, 5, 3)
+
+        # generate output samples
+        samples20 = gen.sample(N_SAMPLES)
+
+        for i in range(N_SAMPLES):
+            sample20 = ' '.join([int2word[x] for x in list(np.array(samples20[i]))])        
+            with open("./data/output/result_epoch" + str(epoch) + "_sample" + str(i) + "_20.txt", "w") as outfile:
+                    outfile.write(sample20)
+                
+        
